6b6840eead54f09468e0364fe0a6f4e6e7baa665
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2020-07-02 21:20:36 -0300

    "Adição de menu"

--- script/popup.js
+++ script/popup.js
@@ -15,6 +15,32 @@
     
     idioma("pt-BR");
 
+    document.querySelector("div.container.seta").addEventListener("click", function(){
+        const DIST = "70px";
+        let setaCont = document.querySelector("div.container.seta");
+        let menuCont = document.querySelector("div.container.menu");
+        let seta = document.querySelector("#seta");
+        if(setaCont.style.marginTop != DIST) {
+            setaCont.style.marginTop = DIST;
+            setaCont.style.paddingBottom = "5px";
+            seta.style.borderBottom = "10px solid #eeeaea";
+            seta.style.borderTop = "1px solid transparent";
+            menuCont.style.height = DIST;
+            menuCont.style.paddingTop = "10px";
+            preencherGaleria(4,"icons", "png", "icons","div.container.menu");
+        } else {
+            setaCont.style.marginTop = "0px";
+            seta.style.borderTop = "10px solid #eeeaea";
+            setaCont.style.paddingBottom = "0px";
+            seta.style.marginTop = "5px";
+            seta.style.borderBottom = "4px solid transparent";
+            menuCont.style.height = "0px";
+            menuCont.innerHTML = '';
+            menuCont.style.paddingTop = "0px"
+        }
+            
+    });
+
     document.querySelector("#trocar").addEventListener("click", () => {
         let input = document.querySelector("#pesquisa");
         if (input.value == '') {
@@ -27,24 +53,24 @@
     document.querySelector("#normal").addEventListener("click", () => {
         enviar("reset");
     });
+
+    document.querySelector("div.container.galeria").addEventListener("click", function(event) {
+        if(event.target.src) {
+            enviar(imagens.item[event.target.id]);
+        }
+    });
     
-    (function () {
-        for (let i = 0; i < imagens.item.length; i++) {
+    function preencherGaleria(quant, endereco, tipo, ident, query) {
+        for (let i = 0; i < quant; i++) {
             let img = document.createElement("IMG");
             img.setAttribute('id',i);
-            img.setAttribute('class','imagens');
-            img.setAttribute('src', "miniaturas/"+i+".jpg");
-            document.querySelector("#containerImg").appendChild(img);
+            img.setAttribute('class',ident);
+            img.setAttribute('src', endereco+"/"+i+"."+tipo);
+            document.querySelector(query).appendChild(img);
         }
-        let galeria = document.getElementsByClassName("imagens");
-        for (let i = 0; i < galeria.length; i++) {
-            galeria[i].addEventListener("click",() => {
-                enviar(imagens.item[i]);
-            });
-        }
-    })();
+    };
     
-    //atulizarImagens();
+    preencherGaleria(imagens.item.length, "miniaturas", "jpg", "imagens","div.container.galeria");
     
     function enviar(base64) {
         let parametros = {
